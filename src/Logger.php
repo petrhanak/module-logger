cd6042425edee23c360321a5bfb0a82fd0a02a90
Yoshi2889 <rick.2889@gmail.com>
2015-12-11 10:53:39 +0100

    Fix Logger namespace
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- src/Logger.php
+++ src/Logger.php
@@ -18,7 +18,7 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-namespace WildPHP\CoreModules;
+namespace WildPHP\CoreModules\Logger;
 
 use Monolog\Handler\ErrorLogHandler;
 use Psr\Log\LoggerInterface;
